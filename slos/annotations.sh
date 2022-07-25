26a7c1ce9751422554fd7e46f367ddd1451985d5
Krzysztof Skitek <krzysztof.skitek@nobl9.com>
2022-07-25 23:27:56 +0200

    impl: Setup sloctl

--- slos/annotations.sh
+++ slos/annotations.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 
-export RELEASE=v0.1.0
 export RELEASE_ESCAPED=$(echo $RELEASE | sed 's/\./-/g')
 export DATE=$(date --iso-8601=seconds)
 
+PROJECT=screeps
+
 touch annotations.yaml
-for SLO_NAME in $(./sloctl --config ~/Downloads/config.toml get slo -p screeps -o json | jq -r .[].metadata.name)
+for SLO_NAME in $(./sloctl get slo -p $PROJECT -o json | jq -r .[].metadata.name)
 do
   export SLO_NAME
   printf "Annotating $SLO_NAME\n"
@@ -13,4 +14,4 @@ do
   echo "---" >> annotations.yaml
 done
 
-cat annotations.yaml
+./sloctl apply -f annotations.yaml
